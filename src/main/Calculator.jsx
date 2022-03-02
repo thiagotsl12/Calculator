354d2970488bcd3c1e72ac2e263a9fd783eb4684
Thiago <thiagotateishi12@gmail.com>
2022-03-02 18:11:49 -0300

    first commit

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import './Calculator.css'
+import Button from '../components/Button'
+import Display from '../components/Display'
+// Botoes que seram renderizados na calculadora
+
+const initialState = {
+    // Valor inicial
+    displayValue: '0',
+    // Se precisa ou não limpar o display
+    clearDisplay: false,
+    // Variavel que vai armazenar a operação
+    operation: null,
+    // Array com dois valores 
+    values: [0, 0],
+    // Parametro que diz se está sendo manipulado o indice 0 ou 1 do array
+    current: 0
+}
+
+export default class Calculator extends Component {
+
+    state = { ...initialState }
+
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+    /* Limpar resultado exibido na calculadora*/
+    clearMemory() {
+        this.setState({ ...initialState })
+    }
+    /* setar operação que foi selecionada*/
+    setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
+    }
+    /* Exibir qual parametro foi selecionado para função da calculadora*/
+    addDigit(n) {
+        // Não deixar digitar o . duas vezes
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        // Sempre que digitar um valor armazenar no array
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="calculator">
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={this.clearMemory} triple />
+                <Button label="/" click={this.setOperation} operation />
+                <Button label="7" click={this.addDigit} />
+                <Button label="8" click={this.addDigit} />
+                <Button label="9" click={this.addDigit} />
+                <Button label="*" click={this.setOperation} operation />
+                <Button label="4" click={this.addDigit} />
+                <Button label="5" click={this.addDigit} />
+                <Button label="6" click={this.addDigit} />
+                <Button label="-" click={this.setOperation} operation />
+                <Button label="1" click={this.addDigit} />
+                <Button label="2" click={this.addDigit} />
+                <Button label="3" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation />
+                <Button label="0" click={this.addDigit} double />
+                <Button label="." click={this.addDigit} />
+                <Button label="=" click={this.setOperation} operation />
+
+            </div>
+        )
+    }
+
+}
